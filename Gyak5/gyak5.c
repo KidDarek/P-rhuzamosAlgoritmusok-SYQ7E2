52e945167e7ece5ae1c6545b71d3495209bb2499
KidDarek <90865592+KidDarek@users.noreply.github.com>
2023-04-24 13:16:24 +0200

    Add files via upload

--- Gyak5/gyak5.c
+++ Gyak5/gyak5.c
@@ -5,30 +5,63 @@
 #include <time.h>
 #include <unistd.h>
 #include <omp.h>
+#include <windows.h>
+
+void LinearFindWaldo(int matrixLength, int matrix[matrixLength][matrixLength][matrixLength])
+{
+    for (int i = 0; i < matrixLength; i++)
+    {
+        for (int j = 0; j < matrixLength; j++)
+        {
+            for (int k = 0; k < matrixLength; k++)
+            {
+                if (matrix[k][j][i] == 1)
+                {
+                    printf("Waldo is on the %d. X value, %d. Y value and %d. Z value", k + 1, j + 1, i + 1);
+                    return;
+                }
+            }
+        }
+    }
+}
 
 int main()
 {
+
+    // generate the matrix
+    srand(time(NULL));
+
     int n;
-    int a = 9;
-    int b = 3;
-    prinf("Size of the matrix: ");
-    scanf("%d",&n);
-int matrix[n][n];
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < n; j++)
+    printf("Size of the matrix: ");
+    scanf("%d", &n);
+    int matrix[n][n][n];
+    int a = rand() % n;
+    int b = rand() % n;
+    int c = rand() % n;
+    for (int i = 0; i < n; i++)
     {
-        if(i == a && j == b)
+        for (int j = 0; j < n; j++)
         {
-            matrix[i][j] = 1;
-        }
-        else
-        {
-matrix[i][j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                if (i == a && j == b && k == c)
+                {
+                    matrix[i][j][k] = 1;
+                }
+                else
+                {
+                    matrix[i][j][k] = 0;
+                }
+            }
         }
     }
-
-}
+    // linear search for the 1 in the matrix
+    clock_t start = clock();
+    printf("\n");
+    LinearFindWaldo(n, matrix);
+    clock_t end = clock();
+    printf("\n");
+    printf("runtime: %d", (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
