f57676e582746b5dfa68c1ec25af43ee7f225a22
KidDarek <madanotsirk@gmail.com>
2023-05-17 19:58:13 +0200

    Create kiskacsa.c

---
+++ Beadandók/kiskacsa.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <omp.h>
+#include <windows.h>
+
+int main()
+{
+    srand(time(NULL));
+    int n;
+    printf("How manny buckets should there be? : ");
+    scanf("%d", &n);
+    int buckets[n];
+    int duckfirstlocaltion = rand() % n;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == duckfirstlocaltion)
+        {
+            buckets[i] = 1;
+        }
+        else
+        {
+            buckets[i];
+        }
+    }
+
+    return 0;
+}
